46347cc793ac70fbb901c7b604ba6b8ddedbc37d
goplanit <goplanit19@gmail.com>
2021-07-09 21:18:36 +0900

    elements files <button, heartButton, image, text> are uploaded

---
+++ src/elements/Image.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Image = (props) => {
+  const { src, size } = props;
+
+  const styles = {
+    src,
+    size,
+  };
+
+  return (
+    <React.Fragment>
+      <ImageDefault {...styles}></ImageDefault>
+    </React.Fragment>
+  );
+};
+
+const ImageDefault = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
+
+export default Image;
