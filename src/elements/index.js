3dfd636ab79e1e64229fb6b1635e6b94128e1e7e
gunwoo8622 <gunwoo8622@naver.com>
2021-07-09 21:47:32 +0900

    update Input, index

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,5 +1,8 @@
 import Grid from './Grid';
+import Image from './Image';
+import Text from './Text';
 import Input from './Input';
+import Button from './Button';
 import Spinner from './Spinner';
 
-export { Grid, Input, Spinner };
+export { Grid, Image, Text, Input, Button, Spinner };
